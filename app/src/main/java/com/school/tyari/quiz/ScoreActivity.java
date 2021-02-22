b6f20b0a9ab918bc81a839ee0f35f0ac22bd8377
sumit <sumits7196@gmail.com>
2021-02-22 14:53:41 +0530

    Updated Quiz

---
+++ app/src/main/java/com/school/tyari/quiz/ScoreActivity.java
@@ -0,0 +1,43 @@
+package com.school.tyari.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.school.tyari.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private TextView score;
+    private Button done;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        score = findViewById(R.id.sa_score);
+        done = findViewById(R.id.sa_done);
+
+        String score_str = getIntent().getStringExtra("SCORE");
+        score.setText(score_str);
+
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ScoreActivity.this,MainQuizActivity.class);
+                ScoreActivity.this.startActivity(intent);
+                ScoreActivity.this.finish();
+
+
+            }
+        });
+
+    }
+}
